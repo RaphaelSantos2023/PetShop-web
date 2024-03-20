6c103e0adf180371c33dd62b4f3b1b1525ccbc02
RaphaelSantos2023 <raphaelsantosoliveira4@gmail.com>
2024-03-20 11:04:12 -0300

    0.8.1

---
+++ Calculo.js
@@ -0,0 +1,5 @@
+const vP1 = document.getElementById("valor1").textContent()
+const vP2 = document.getElementById("valor2").textContent()
+
+const valorTotal = vP1 + vP2
+document.getElementById("valorTotal").innerHTML =  vP1 + vP2
